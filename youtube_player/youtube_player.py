acddad9425e367c99c9716ca4739a344f7439daf
Abhinav Anand <abhinavanand1905@gmail.com>
2017-10-01 10:03:37 +0530

    enter a song's name, it will get played on youtube
    for ex - input :- shape of you
    shape of you will get played on YouTube

---
+++ youtube_player/youtube_player.py
@@ -0,0 +1,23 @@
+import os
+import webbrowser
+os.environ["HTTPS_PROXY"] = "http://username:pass@192.168.1.107:3128"
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+query=input('Enter the song to be played: ')
+query=query.replace(' ','+')
+
+# Search for the song youtube
+url='https://www.youtube.com/results?search_query='+query
+source_code = requests.get(url, headers=headers, timeout=15)
+plain_text=source_code.text
+soup=BeautifulSoup(plain_text,"html.parser")
+
+# scrape the link of first video with that song name
+songs=soup.findAll('div',{'class':'yt-lockup-video'})
+song=songs[0].contents[0].contents[0].contents[0]
+link=song['href']
+
+# play it by visiting the link
+webbrowser.open('https://www.youtube.com'+link)

fac31e9d7cbad6b3df144e402db6450d3a20c5c3
Taniyasingh <taniya87c@gmail.com>
2017-10-17 19:35:26 +0530

    Clean up

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
 import socket
-host="127.0.0.1"
-port=12345
+host = "127.0.0.1"
+port = 12345
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
-print("ENTER NO TO BE SENT:")
-a=raw_input()
+print ("ENTER NO TO BE SENT:")
+a = raw_input()
 s.sendall(a)
 data=s.recv(1024)
 s.close()
